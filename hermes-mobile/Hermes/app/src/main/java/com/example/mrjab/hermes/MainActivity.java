0782a1888e52ae1dc84724efdd9ec32af8796a4d
MRJAB <halil_jabir_1992@hotmail.com>
2017-02-06 23:42:35 +0000

    initial-progress

---
+++ hermes-mobile/Hermes/app/src/main/java/com/example/mrjab/hermes/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.mrjab.hermes;
+
+import android.app.usage.UsageEvents;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.EventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+    }
+
+    public void signupPage_click(View e)
+    {
+        setContentView(R.layout.sign_up);
+    }
+
+    public void chatListPage_click(View e)
+    {
+        setContentView(R.layout.user_hp);
+    }
+
+    public void InitHomePage()
+    {
+    }
+}
